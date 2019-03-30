21408ca94a2ae4600c1e631ae2441ff9a62e3f3f
Pål Grønås Drange <PGDR@statoil.com>
2019-03-30 14:19:48 +0100

    Use temp dir

--- textrain/__init__.py
+++ textrain/__init__.py
@@ -7,42 +7,13 @@ import contextlib
 ## UTIL
 
 @contextlib.contextmanager
-def tmp(path=None, teardown=True):
-    """Create and go into tmp directory, returns the path.
-
-    This function creates a temporary directory and enters that directory.  The
-    returned object is the path to the created directory.
-
-    If @path is not specified, we create an empty directory, otherwise, it must
-    be a path to an existing directory.  In that case, the directory will be
-    copied into the temporary directory.
-
-    If @teardown is True (defaults to True), the directory is (attempted)
-    deleted after context, otherwise it is kept as is.
-
-    """
+def tmp():
     cwd = os.getcwd()
-    fname = tempfile.NamedTemporaryFile().name
-
-    if path:
-        if not os.path.isdir(path):
-            raise IOError('No such directory: %s' % path)
-        shutil.copytree(path, fname)
-    else:
-        # no path to copy, create empty dir
-        os.mkdir(fname)
-
-    os.chdir(fname)
-
-    yield fname  # give control to caller scope
-
+    with tempfile.TemporaryDirectory() as tmpdir:
+        name = tmpdir
+        os.chdir(name)
+        yield name  # give control to caller scope
     os.chdir(cwd)
-
-    if teardown:
-        try:
-            shutil.rmtree(fname)
-        except OSError as oserr:
-            shutil.rmtree(fname, ignore_errors=True)
 
 
 def _format(text, replacement):
